33e0f75edba4ce70d5aed3d44219e1e19319ea10
QA tlr <toleradan@yahoo.com>
2025-04-22 09:33:21 +0800

    test

---
+++ Utility/Filework.py
@@ -0,0 +1,10 @@
+# Reading data from the file
+
+obj = open('../../../Documents/Notes/Testing of BPMN2.txt', 'r')
+
+
+#Read all data from the file
+# print(obj.read())
+
+#Read specific line from the file
+print(obj.readline(9,))
